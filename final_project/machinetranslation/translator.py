c7e0b910eeffdf4cf3205c1ecfc574f9ce2221b0
cortedteate <carlos.cortesosorio@teatedigital.com>
2022-11-16 09:54:28 -0500

    Temporary changes with methods

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -7,12 +7,21 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(version='2018-05-01', authenticator=authenticator)
+language_translator.set_service_url(url)
 
 def englishToFrench(englishText):
     #write the code here
+translation = language_translator.translate(
+    text=englishText,
+    model_id='en-fr').get_result()
+    frenchText = translate['Translations'][0]['Translation']
     return frenchText
 
 def frenchToEnglish(frenchText):
     #write the code here
+    text=frenchText,
+    model_id=('fr-en').get_result()
+    englishText = translate['Translations'][0]['Translation']
     return englishText
